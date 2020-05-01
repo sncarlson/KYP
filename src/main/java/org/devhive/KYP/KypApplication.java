a47c337bc49b2c218cea6fbd11921ac4f0026d73
Steven Carlson <sncarlson@dev-hive.org>
2020-05-01 16:09:30 -0400

    Initial commit

---
+++ src/main/java/org/devhive/KYP/KypApplication.java
@@ -0,0 +1,13 @@
+package org.devhive.KYP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KypApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/devhive/KYP/KypApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devhive.KYP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
